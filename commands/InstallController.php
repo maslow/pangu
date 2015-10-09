c992e24aa6a0708dcb71c6c215cb67e812b7d123
maslow <wangfugen@126.com>
2015-10-09 22:34:29 +0800

    重写配置本地化机制并修改相应的Install命令行功能;

--- commands/InstallController.php
+++ commands/InstallController.php
@@ -12,17 +12,23 @@ class InstallController extends Controller
         $configPath = \Yii::getAlias('@app/config');
         $dbExample = $configPath . '/db-example.php';
         $dbPath = $configPath . '/db.php';
-        $devLock = $configPath . '/dev.lock';
+
+        $webroot = \Yii::getAlias('@app/web');
+        $indexDev = $webroot.'/index-dev.php';
+        $indexProd = $webroot.'/index-prod.php';
+        $index = $webroot.'/index.php';
 
         echo "Clear Env...";
         file_exists($dbPath) ? unlink($dbPath) : print('x..');
-        file_exists($devLock) ? unlink($devLock) : print('x..');
+        file_exists($index) ? unlink($index) : print('x..');
         echo "Done!\n";
 
 
         file_put_contents($dbPath, file_get_contents($dbExample)) ? print($dbPath . "\n") : print("error!");
         if ($env == 'dev') {
-            file_put_contents($devLock, 'Development Env') ? print($devLock . "\n") : print("error!");
+            file_put_contents($index,file_get_contents($indexDev))?print($index." to Dev Evn. \n"):print('error!');
+        }else{
+            file_put_contents($index,file_get_contents($indexProd))?print($index." to Prod Evn. \n"):print('error!');
         }
     }
 }
