758b17ceaf6b3f5383900424ff3b8505ec8c0c39
maslow <wangfugen@126.com>
2015-10-09 22:35:35 +0800

    调整代码格式;

--- commands/InstallController.php
+++ commands/InstallController.php
@@ -14,21 +14,20 @@ class InstallController extends Controller
         $dbPath = $configPath . '/db.php';
 
         $webroot = \Yii::getAlias('@app/web');
-        $indexDev = $webroot.'/index-dev.php';
-        $indexProd = $webroot.'/index-prod.php';
-        $index = $webroot.'/index.php';
+        $indexDev = $webroot . '/index-dev.php';
+        $indexProd = $webroot . '/index-prod.php';
+        $index = $webroot . '/index.php';
 
         echo "Clear Env...";
         file_exists($dbPath) ? unlink($dbPath) : print('x..');
         file_exists($index) ? unlink($index) : print('x..');
         echo "Done!\n";
 
-
         file_put_contents($dbPath, file_get_contents($dbExample)) ? print($dbPath . "\n") : print("error!");
         if ($env == 'dev') {
-            file_put_contents($index,file_get_contents($indexDev))?print($index." to Dev Evn. \n"):print('error!');
-        }else{
-            file_put_contents($index,file_get_contents($indexProd))?print($index." to Prod Evn. \n"):print('error!');
+            file_put_contents($index, file_get_contents($indexDev)) ? print($index . " to Dev Evn. \n") : print('error!');
+        } else {
+            file_put_contents($index, file_get_contents($indexProd)) ? print($index . " to Prod Evn. \n") : print('error!');
         }
     }
 }
