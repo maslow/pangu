30ba75faaf75740cc57f2b465fb89d4db4247e6b
maslow <wangfugen@126.com>
2015-10-20 11:35:29 +0800

    config/web.php 中语言设置，改为大小写敏感，以适应不同的操作系统环境。

--- config/web.php
+++ config/web.php
@@ -6,7 +6,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'language' => 'zh-cn',
+    'language' => 'zh-CN',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
