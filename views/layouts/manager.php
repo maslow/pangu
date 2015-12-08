9a025143b5edac5e4091e41a6d6df068557c7210
maslow <wangfugen@126.com>
2015-12-08 23:19:58 +0800

    修改prompt显示的方式（使用layer)

--- views/layouts/manager.php
+++ views/layouts/manager.php
@@ -7,7 +7,14 @@ use yii\widgets\Breadcrumbs;
 /* @var $content string */
 
 \yii\bootstrap\BootstrapAsset::register($this);
+\app\assets\LayerAsset::register($this);
 ?>
+<script>
+    <?php $this->beginBlock('prompt_js') ?>
+    layer.msg('<?=Yii::$app->session->getFlash(Yii::$app->params['prompt.param.backend'])?>');
+    <?php $this->endBlock() ?>
+</script>
+
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
@@ -19,17 +26,19 @@ use yii\widgets\Breadcrumbs;
     <?php $this->head() ?>
 </head>
 <body>
-
 <?php $this->beginBody() ?>
 <div class="wrap">
     <?php
     if (Yii::$app->session->hasFlash(Yii::$app->params['prompt.param.backend'])) {
-        echo \yii\bootstrap\Alert::widget([
+         $this->registerJs($this->blocks['prompt_js']);
+         /*
+          * \yii\bootstrap\Alert::widget([
             'options' => [
                 'class' => 'alert-warning',
             ],
             'body' => Yii::$app->session->getFlash(Yii::$app->params['prompt.param.backend']),
         ]);
+         */
     }
     ?>
     <?= $content ?>
